23032aabe20b6a2653533ca8fe61898efc93f9b1
Adam Jackson <akjackson1@gmail.com>
2025-09-07 15:07:31 -0400

    Fix Mapbox build error

---
+++ app.config.js
@@ -0,0 +1,68 @@
+module.exports = ({ config }) => ({
+  ...config,
+  expo: {
+    ...(config.expo || {}),
+    name: "OpenBirding",
+    slug: "OpenBirding",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "openbirding",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+      bundleIdentifier: "com.rawcomposition.openbirding",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+      permissions: ["INTERNET"],
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      [
+        "@rnmapbox/maps",
+        {
+          // 👇 Now the plugin receives the actual secret from env
+          RNMapboxMapsDownloadToken: process.env.MAPBOX_DOWNLOADS_TOKEN || process.env.MAPBOX_DOWNLOAD_TOKEN,
+          RNMapboxMapsImpl: "mapbox",
+        },
+      ],
+      [
+        "expo-location",
+        {
+          locationWhenInUsePermission:
+            "This app uses location to show your position on the map and find nearby hotspots.",
+        },
+      ],
+    ],
+    experiments: { typedRoutes: true },
+    extra: {
+      router: {},
+      eas: { projectId: "2944a151-98b6-4d2a-9104-65facf9def35" },
+      // Runtime token you'll read in JS
+      MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
+    },
+  },
+});
